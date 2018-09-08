2e15a176b859f690a0a6405cf2087a6a97425db2
Chris Cole <38804064+oakstwest@users.noreply.github.com>
2018-09-08 14:46:32 -0700

    Add files via upload

--- src/components/Video/Render_Video.js
+++ src/components/Video/Render_Video.js
@@ -1,34 +1,47 @@
 require('dotenv').config();
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from ' youtube-api-search';
 import SearchBar from './components/Video/Search_Bar';
 import VideoList from './components/Video/Video_List';
+import VideoDetail from './components/Video/Video_Detail';
 
 
 const {
     REACT_YOUTUBE_API_KEY
 } = process.env
 
-
-
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('surfboards');
+    }
 
-        YTSearch({ key: REACT_YOUTUBE_API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({ videos })
+    videoSearch(term) {
+        YTSearch({ key: REACT_YOUTUBE_API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
-
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
         return (
             <div>
-                <SearchBar />
-                <VideoList videos = {this.state.videos}/>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
             </div>
         )
     }
--- src/components/Video/Video_List.js
+++ src/components/Video/Video_List.js
@@ -2,13 +2,16 @@ import React from 'react';
 import VideoListItem from './Video_List_Item'
 
 const VideoList = (props) => {
-   const videoItems = props.video.map((video) => {
-        return <VideoListItem video={video} />
+    const videoItems = props.video.map((video) => {
+        return <VideoListItem
+            onVideoSelect={props.onVideoSelect}
+            key={video.etag}
+            video={video} />
     });
 
     return (
         <ul className="col-mid-4 list-group">
-           {videoItems}
+            {videoItems}
         </ul>
     );
 };
